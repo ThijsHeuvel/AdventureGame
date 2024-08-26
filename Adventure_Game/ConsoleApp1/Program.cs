76cb18cc91b3e41ff104995b4a20d484cdbd3e91
ThijsHeuvel <d294396@edu.curio.nl>
2024-08-26 10:53:49 +0200

    Made a test file, and made the program Object Oriented.

--- Adventure_Game/ConsoleApp1/Program.cs
+++ Adventure_Game/ConsoleApp1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdventureGame;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            TextGame game = new TextGame();
+            game.Run();
+        }
+    }   
+}
---
+++ Adventure_Game/ConsoleApp1/TextGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    internal class TextGame
+    {
+        internal void Run() // Gets called when the program starts in Program.cs.
+        {
+            string dirPath = Environment.CurrentDirectory;
+            Console.WriteLine(dirPath);
+
+            Console.WriteLine("Welcome to AdventureGame!");
+            Console.WriteLine("1. New Game");
+            // Add if statement to check for Save-Game file
+
+            if (File.Exists($"{dirPath}\\savegame.json"))
+            {
+                Console.WriteLine("2. Load Game");
+            }
+        }
+    }
+}
