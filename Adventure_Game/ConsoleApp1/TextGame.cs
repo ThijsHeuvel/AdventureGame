8af337e17d217f2de0571e0bb787137ad95bed0b
ThijsHeuvel <d294396@edu.curio.nl>
2024-08-28 10:18:49 +0200

    Got saving system to work. Now all I need to do is add a loading system.

--- Adventure_Game/ConsoleApp1/TextGame.cs
+++ Adventure_Game/ConsoleApp1/TextGame.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,15 +9,19 @@ namespace AdventureGame
 {
     internal class TextGame
     {
+        string? dirPath;
         internal void Run() // Gets called when the program starts in Program.cs.
         {
-            string dirPath = Environment.CurrentDirectory;
+            dirPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            MainMenu();
+        }
 
+        private void MainMenu() // Shows the main menu of the application
+        {
             Console.WriteLine("Welcome to AdventureGame!");
             Console.WriteLine("1. New Game");
-            // Add if statement to check for Save-Game file
 
-            if (File.Exists($"{dirPath}\\savegame.json"))
+            if (File.Exists($"{dirPath}\\save.txt"))
             {
                 Console.WriteLine("2. Load Game");
             }
